70846a0e40ae02b94142dba776019cddc2f45533
Ashvansadri <Sadri.sadegh@gmail.com>
2025-06-08 10:43:07 +0330

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+// frontend/src/App.jsx
+function App() {
+  return (
+    <div>
+      <h1>SmartImmigrate Frontend</h1>
+      <p>Welcome to SmartImmigrate!</p>
+    </div>
+  );
+}
+
+export default App;
